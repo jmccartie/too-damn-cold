3e9ccc857987bd20826fe7b68bb16f7bd03da1d7
Jon McCartie <jon@mccartie.com>
2018-02-23 16:57:18 -0800

    fixed bug where distance could be blank

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-    if params[:origin] && params[:temperature]
+    if params[:origin].present? && params[:temperature].present?
       @origin = Geocoder.search(params[:origin]).first
-      @results = City.where('temperature >= ?', params[:temperature]).near(@origin.coordinates, params[:distance] || 500).limit(10)
+      @results = City.where('temperature >= ?', params[:temperature]).near(@origin.coordinates, params[:distance].presence || 500).limit(10)
     end
   end
 
